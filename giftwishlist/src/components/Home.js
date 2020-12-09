2da8ce23d551c9c90868eb289c4999d4ae0912e3
veeepi <vpreduna90@gmail.com>
2020-12-09 07:31:58 -0800

    #3 create Home Component

---
+++ giftwishlist/src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to WishBucket!</h1>
+            </div>
+        )
+    }
+}
