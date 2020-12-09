647b6d0b224b5ad396acfd71145d7a9b0f67e110
russtelen <rtelen@my.bcit.ca>
2020-12-09 13:21:45 -0800

    Create Register component #26

---
+++ giftwishlist/src/components/Register.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Register Page Works</h1>
+    </div>
+  );
+};
+
+export default Register;
