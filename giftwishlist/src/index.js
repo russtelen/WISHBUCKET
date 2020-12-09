72637c16c5a6152ab66d982fa811922a9a2a23e8
veeepi <vpreduna90@gmail.com>
2020-12-09 06:03:34 -0800

    #9: npm install jquery popper.js

--- giftwishlist/src/index.js
+++ giftwishlist/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
