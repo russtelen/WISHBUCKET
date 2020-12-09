357fd9a5df89043d4d746010292bb384eaad0984
russtelen <rtelen@my.bcit.ca>
2020-12-09 11:42:26 -0800

    Add overview in landing #13

--- giftwishlist/src/components/Home.js
+++ giftwishlist/src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { ImGift } from "react-icons/im";
 import { NavLink } from "react-router-dom";
+import { IoStorefrontOutline } from "react-icons/io5";
+import { GiStairsCake, GiTakeMyMoney } from "react-icons/gi";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
-        <div className="jumbotron home__jumbotron-custom">
+        {/* jumbotron */}
+        <div className="home__jumbotron-custom jumbotron">
           <h1 className="display-4">
             <ImGift className="mr-2 my-2" />
             WishBucket
@@ -22,6 +25,33 @@ export default class Home extends Component {
               Create Wish List
             </NavLink>
           </p>
+        </div>
+        {/* Quick Overview */}
+        <div className="home__overview">
+          <div className="home__overview-cards container row">
+            <div className="home__overview-card col-sm-12 col-md-3 mx-auto my-2">
+              <IoStorefrontOutline size={50} />
+              <p className="home__overview-header">Add Anything</p>
+              <p className="home__overview-subheader">
+                Add gifts from any store online, or in the street
+              </p>
+            </div>
+            <div className="home__overview-card col-sm-12 col-md-3 mx-auto my-2">
+              <GiStairsCake size={50} />
+              <p className="home__overview-header">Any Occasion</p>
+              <p className="home__overview-subheader">
+                Birthdays, Baby Showers, Christmas.. WishBucket works great
+                everytime
+              </p>
+            </div>
+            <div className="home__overview-card col-sm-12 col-md-3 mx-auto my-2">
+              <GiTakeMyMoney size={50} />
+              <p className="home__overview-header">Completely Free</p>
+              <p className="home__overview-subheader">
+                WishBucket is completely free and completely unlimited
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
