615b2a7a2e934a5f3b2ce83a239baf070125d19b
russtelen <rtelen@my.bcit.ca>
2020-12-09 13:30:16 -0800

    Add starter register form, states and animations

--- giftwishlist/src/components/auth/Register.js
+++ giftwishlist/src/components/auth/Register.js
@@ -1,11 +1,78 @@
 import React, { Component } from "react";
+import "animate.css";
 
 class Register extends Component {
+  //state variables for form inputs and errors
+  state = {
+    email: "",
+    password: "",
+    confirmpassword: "",
+  };
+
+  handleSubmit = async (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    //Perform Validation here
+
+    //Integrate Auth here on valid form submission
+  };
+
+  onInputChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  };
   render() {
     return (
-      <div>
-        <h1>Register Page works</h1>
-      </div>
+      <section className="section auth animate__animated animate__fadeInDown">
+        <div className="container">
+          <h1 className="display-4 mb-4">Register</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="field">
+              <p className="control">
+                <input
+                  className="input"
+                  type="email"
+                  id="email"
+                  placeholder="Enter email"
+                  value={this.state.email}
+                  onChange={this.onInputChange}
+                />
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <input
+                  className="input"
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.onInputChange}
+                />
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <input
+                  className="input"
+                  type="password"
+                  id="confirmpassword"
+                  placeholder="Confirm password"
+                  value={this.state.confirmpassword}
+                  onChange={this.onInputChange}
+                />
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <button className="button is-success">Register</button>
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
     );
   }
 }
