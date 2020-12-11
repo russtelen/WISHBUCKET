73e5a79094e97a7fc735abecf6b15af120eaee98
russtelen <rtelen@my.bcit.ca>
2020-12-11 15:34:05 -0800

    initialize state variables #60

--- giftwishlist/src/components/wishlist/Wishlist.js
+++ giftwishlist/src/components/wishlist/Wishlist.js
@@ -1,41 +1,46 @@
 import React, { useState, useEffect } from "react";
-import Item from './item/Item';
-import wishlistService from '../../services/wishlists.js';
+import Item from "./item/Item";
+import wishlistService from "../../services/wishlists.js";
 
 export default function Wishlist({ match }) {
   const [wishlist, setWishlist] = useState([]);
   const [createdItem, setCreatedItem] = useState({});
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [purchaseLink, setPurchaseLink] = useState("");
+  const [price, setPrice] = useState("");
+  const [complete, setComplete] = useState(false);
 
   // Taken from the url
   const wishlistId = match.params.id;
 
   const fetchWishlists = () => {
-    wishlistService.getById(wishlistId)
-      .then(response =>
-        setWishlist(response))
-      .catch(error => console.log(error))
+    wishlistService
+      .getById(wishlistId)
+      .then((response) => setWishlist(response))
+      .catch((error) => console.log(error));
   };
 
   const createItem = () => {
-    wishlistService.createItem(createdItem, wishlistId)
-      .then(response => console.log(response))
-      .catch(error => console.log(error))
-  }
+    wishlistService
+      .createItem(createdItem, wishlistId)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
 
-  const handleNameChange = e => {
-    setCreatedItem(
-      {
+  const handleNameChange = (e) => {
+    setCreatedItem({
       ...createdItem,
-      name: e.target.value
-      }
-    )
-  }
+      name: e.target.value,
+    });
+  };
 
   useEffect(() => {
     fetchWishlists();
   }, []); // empty [] dependancy list to stop infinite loop
 
- return (
+  return (
     <div>
       <table className="table is-fullwidth">
         <thead>
@@ -49,24 +54,24 @@ export default function Wishlist({ match }) {
           </tr>
         </thead>
         <tbody>
-          {wishlist.items ? wishlist.items.map(w => <Item key={w.name} item={w}/>) : ''}
+          {wishlist.items
+            ? wishlist.items.map((w) => <Item key={w.name} item={w} />)
+            : ""}
         </tbody>
       </table>
 
       <div className="container has-text-centered">
-      <input
-        placeholder="Wishlist Name"
-        type="text"
-        value={createdItem.name}
-        onChange={handleNameChange}
-      />
+        <input
+          placeholder="Wishlist Name"
+          type="text"
+          value={createdItem.name}
+          onChange={handleNameChange}
+        />
 
-      <button className="button" onClick={createItem}>
-        Create Wishlist
-      </button>
+        <button className="button" onClick={createItem}>
+          Create Wishlist
+        </button>
       </div>
-
-
     </div>
   );
 }
