c31d55b05b96e953c0b5ba21fa8891752ff0b26b
russtelen <rtelen@my.bcit.ca>
2020-12-14 17:11:48 -0800

    FIXED UPDATE WISHLIST #55

--- giftwishlist/src/components/Wishlists.js
+++ giftwishlist/src/components/Wishlists.js
@@ -8,6 +8,7 @@ export default function Wishlists() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [dueDate, setDueDate] = useState("");
+  const [id, setId] = useState(0);
   const [showInputs, setShowInputs] = useState(false);
   const [editNameInput, setEditNameInput] = useState("");
   const [editPasswordInput, setEditPasswordInput] = useState("");
@@ -83,7 +84,7 @@ export default function Wishlists() {
   };
 
   //Update Wishlist (PUT)
-  const updateWishlist = (name, password, dueDate) => {
+  const updateWishlist = (id, name, password, dueDate) => {
     fetch(BASE_URL + "wishlist", {
       method: "PUT",
       headers: {
@@ -92,6 +93,7 @@ export default function Wishlists() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        Id: id,
         Name: name,
         Password: password,
         DueDate: dueDate,
@@ -109,6 +111,12 @@ export default function Wishlists() {
       });
   };
 
+  const handleSubmit = () => {
+    updateWishlist(id, editNameInput, editPasswordInput, editDateInput);
+
+    console.log("Wishlist Updated");
+  };
+
   const handleNameChangeEdit = (e) => {
     setEditNameInput(e.target.value);
   };
@@ -121,9 +129,12 @@ export default function Wishlists() {
     setEditDateInput(e.target.value);
   };
 
-  const showEditInputs = (name, password, duedate) => {
+  const showEditInputs = (id, name, password, duedate) => {
     //Hide/show inputs
     showInputs ? setShowInputs(false) : setShowInputs(true);
+
+    setId(id);
+    console.log(id);
 
     //Set name input
     setEditNameInput(name);
@@ -211,12 +222,7 @@ export default function Wishlists() {
           value={editDateInput}
           onChange={handleDueDateChangeEdit}
         />
-        <button
-          className="button"
-          onClick={() => {
-            console.log("Wishtlist Updated");
-          }}
-        >
+        <button className="button" onClick={handleSubmit}>
           Submit
         </button>
       </div>
@@ -254,6 +260,7 @@ export default function Wishlists() {
                   className="button is-info is-light"
                   onClick={() =>
                     showEditInputs(
+                      wishlist.id,
                       wishlist.name,
                       wishlist.password,
                       wishlist.dueDate
