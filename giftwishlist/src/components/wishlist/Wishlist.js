29e75f36cbe36efa5a875243c85fce7033cca3ef
russtelen <rtelen@my.bcit.ca>
2020-12-16 15:19:08 -0800

    Make table responsive #118

--- giftwishlist/src/components/wishlist/Wishlist.js
+++ giftwishlist/src/components/wishlist/Wishlist.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import Item from "./item/Item";
 import wishlistService from "../../services/wishlists.js";
 import { NavLink } from "react-router-dom";
-import { UserAuthContext } from '../UserAuthContext';
-
+import { UserAuthContext } from "../UserAuthContext";
 
 export default function Wishlist({ match }) {
   const [wishlist, setWishlist] = useState([]);
@@ -16,7 +15,7 @@ export default function Wishlist({ match }) {
 
   // Taken from the url
   const wishlistId = match.params.id;
-  const password = new URLSearchParams(window.location.search).get('password');
+  const password = new URLSearchParams(window.location.search).get("password");
   const BASE_URL = process.env.REACT_APP_BASE_URL + "api/";
 
   const { userAuthenticated } = useContext(UserAuthContext);
@@ -70,19 +69,17 @@ export default function Wishlist({ match }) {
       },
     })
       .then((res) => res.json())
-    // Data retrieved.
+      // Data retrieved.
       .then((data) => {
         //Could just update the state locally without another fetch
-        window.location.href = "/wishlist/"
+        window.location.href = "/wishlist/";
       })
-    // Data not retrieved.
+      // Data not retrieved.
       .catch((e) => {
         console.log(e);
       });
-    
-  }
+  };
   const deleteItem = (wishlistId, itemId) => {
-
     fetch(BASE_URL + "wishlist/" + wishlistId + "/item/" + itemId, {
       method: "DELETE",
       headers: {
@@ -92,118 +89,121 @@ export default function Wishlist({ match }) {
       },
     })
       .then((res) => res.json())
-    // Data retrieved.
+      // Data retrieved.
       .then((data) => {
         //Unnecessary fetch
         fetchWishlists();
       })
-    // Data not retrieved.
+      // Data not retrieved.
       .catch((e) => {
         console.log(e);
       });
-  }
+  };
 
   const shareWishlist = () => {
     setShowShareNotification(!showShareNotification);
-    const dummy = document.createElement('input'),
-        text = window.location.href;
+    const dummy = document.createElement("input"),
+      text = window.location.href;
 
     document.body.appendChild(dummy);
     dummy.value = text;
     dummy.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(dummy);
-  }
-
+  };
 
   return (
-    <div className="table-container">
-      <NavLink to="/wishlist">
-        Return to Wishlists
-      </NavLink>
-      <table className="table is-fullwidth">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Image</th>
-            <th>Purchase Link</th>
-            <th>Price</th>
-            <th>Completed</th>
-            <th>Update</th>
-            <th>Delete Item</th>
-          </tr>
-        </thead>
-        <tbody>
-          {wishlist.items ? wishlist.items.map((w) =>
-            <tr key={w.id}>
-              <Item item={w}/>
-              <td>
-                <button className="button" onClick={() => deleteItem(w.wishlistID, w.id)}>
-                  Delete
-                </button>
-              </td>
-            </tr>)
-              : null }
-        </tbody>
+    <div>
+      <NavLink to="/wishlist">Return to Wishlists</NavLink>
+      <div className="table-responsive container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Image</th>
+              <th>Purchase Link</th>
+              <th>Price</th>
+              <th>Completed</th>
+              <th>Update</th>
+              <th>Delete Item</th>
+            </tr>
+          </thead>
+          <tbody>
+            {wishlist.items
+              ? wishlist.items.map((w) => (
+                  <tr key={w.id}>
+                    <Item item={w} />
+                    <td>
+                      <button
+                        className="button"
+                        onClick={() => deleteItem(w.wishlistID, w.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
+        </table>
+      </div>
+      {userAuthenticated ? (
+        <div className="container has-text-centered wishlist-inputs">
+          <input
+            placeholder="Item Name"
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <input
+            placeholder="Description"
+            type="text"
+            value={description}
+            onChange={handleDescChange}
+          />
+          <input
+            placeholder="Image Link"
+            type="url"
+            value={image}
+            onChange={handleImageChange}
+          />
+          <input
+            placeholder="Purchase Link"
+            type="url"
+            value={purchaseLink}
+            onChange={handlePurchaseLinkChange}
+          />
+          <input
+            placeholder="Price"
+            type="text"
+            value={price}
+            onChange={handlePriceChange}
+          />
 
-      </table>
-      {userAuthenticated ?
-       <div className="container has-text-centered wishlist-inputs">
-         <input
-           placeholder="Item Name"
-           type="text"
-           value={name}
-           onChange={handleNameChange}
-         />
-         <input
-           placeholder="Description"
-           type="text"
-           value={description}
-           onChange={handleDescChange}
-         />
-         <input
-           placeholder="Image Link"
-           type="url"
-           value={image}
-           onChange={handleImageChange}
-         />
-         <input
-           placeholder="Purchase Link"
-           type="url"
-           value={purchaseLink}
-           onChange={handlePurchaseLinkChange}
-         />
-         <input
-           placeholder="Price"
-           type="text"
-           value={price}
-           onChange={handlePriceChange}
-         />
-
-         <button className="button" onClick={createItem}>
-           Add Item
-         </button>
-         <button className="button is-danger" onClick={deleteWishlist}>
-           Delete Wishlist
-         </button>
-       </div>
-       : 'Log in to edit'}
+          <button className="button" onClick={createItem}>
+            Add Item
+          </button>
+          <button className="button is-danger" onClick={deleteWishlist}>
+            Delete Wishlist
+          </button>
+        </div>
+      ) : (
+        "Log in to edit"
+      )}
 
       <div>
-        {showShareNotification ? <div class="notification is-primary is-light wishlist-notification">
-                                   <button class="delete" onClick={shareWishlist}></button>
-                                   <a href={window.location.href}>Link</a> copied to clipboard.
-                                 </div>
-         :
-         <button className="button" onClick={shareWishlist}>
-           Share Wishlist
-         </button>
-        }
-
+        {showShareNotification ? (
+          <div class="notification is-primary is-light wishlist-notification">
+            <button class="delete" onClick={shareWishlist}></button>
+            <a href={window.location.href}>Link</a> copied to clipboard.
+          </div>
+        ) : (
+          <button className="button" onClick={shareWishlist}>
+            Share Wishlist
+          </button>
+        )}
       </div>
-
-
     </div>
   );
 }
