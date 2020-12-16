fb2e4cc935b59934a28a85e350dfcf59f6de7610
fatmabadri <fbadri@my.bcit.ca>
2020-12-16 15:12:55 -0800

    Modified Footer Component

--- giftwishlist/src/components/Footer.js
+++ giftwishlist/src/components/Footer.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Footer extends Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+   return (
+     <footer className="pt-4 my-md-5 pt-md-5">
+       <div className="row">
+          <div className="col-6 col-md">
+              <h5>Company</h5>
+          </div> 
+
+          <div className="col-6 col-md">
+              <h5>Features</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Cool stuff</a></li>
+                <li><a class="link-secondary" href="#">Random feature</a></li>
+              </ul>
+          </div> 
+
+          <div className="col-6 col-md">
+              <h5>Resources</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Cool stuff</a></li>
+                <li><a class="link-secondary" href="#">Random feature</a></li>
+              </ul>
+          </div>
+
+          <div className="col-6 col-md">
+              <h5>Solutions</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Cool stuff</a></li>
+                <li><a class="link-secondary" href="#">Random feature</a></li>
+              </ul>
+          </div>
+       </div>
+     </footer>
+   );
   }
-}
+} 
