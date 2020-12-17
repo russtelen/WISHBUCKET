0406755ed631e4288f316c3fa6d9a4d455c55e10
fatmabadri <fbadri@my.bcit.ca>
2020-12-16 16:21:19 -0800

    Add company icon in the footer section

--- giftwishlist/src/components/Footer.js
+++ giftwishlist/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class Footer extends Component {
   render() {
@@ -6,7 +7,14 @@ export default class Footer extends Component {
      <footer className="pt-4 my-md-5 pt-md-5">
        <div className="row">
           <div className="col-6 col-md">
-              <h5>Company</h5>
+                <NavLink to="/">
+                <img src={"https://vectr.com/vleddepruna/aVozy6qf2.svg?width=224&height=236&select=aVozy6qf2page0"}
+                  width="30em"
+                />
+                <h6>WishBucket</h6>
+              </NavLink>
+              {/* <h5>Company</h5> */}
+              <small class="d-block mb-3 text-muted">&copy; 2015-2020</small>
           </div> 
 
           <div className="col-6 col-md">
