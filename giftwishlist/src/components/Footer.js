c3ac4cec9d3e8e5826e20e57d8936a4f9f79a8ef
fatmabadri <fbadri@my.bcit.ca>
2020-12-17 12:23:34 -0800

    Modified the background color of footer

--- giftwishlist/src/components/Footer.js
+++ giftwishlist/src/components/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends Component {
       <footer className="pt-4 my-md-5 mb-0 pb-0 pt-md-5">
          <div className="row">
           <div className="col-6 col-md">
-                <h6>WISHBUCKET</h6>
+                <h6 className="text-dark">WISHBUCKET</h6>
               <ul class="list-unstyled text-small">
                 <li><a className="link-secondary" href="#">Our Story</a></li>
                 <li><a className="link-secondary" href="#">Our People</a></li>
@@ -17,7 +17,7 @@ export default class Footer extends Component {
           </div> 
 
           <div className="col-6 col-md mb-3">
-              <h6>FEATURES</h6>
+              <h6 className="text-dark">FEATURES</h6>
               <ul class="list-unstyled text-small">
                 <li><a className="link-secondary" href="#">Cool stuff</a></li>
                 <li><a className="link-secondary" href="#">Share With Anyone</a></li>
@@ -27,7 +27,7 @@ export default class Footer extends Component {
           </div> 
 
           <div className="col-6 col-md mb-3">
-              <h6>RESOURCES</h6>
+              <h6 className="text-dark">RESOURCES</h6>
               <ul className="list-unstyled text-small">
                 <li><a className="link-secondary" href="#">iPhone WishList IOS  App</a></li>
                 <li><a className="link-secondary" href="#">Android WishList App</a></li>
@@ -37,7 +37,7 @@ export default class Footer extends Component {
           </div>
 
           <div className="col-6 col-md mb-3">
-              <h6>DESIGN &amp; DEVELOPMENT</h6>
+              <h6 className="text-dark">DESIGN &amp; DEVELOPMENT</h6>
               <ul className="list-unstyled text-small">
               <li><a className="link-secondary" href="#">Stefan Kuznetsov </a></li>
                 <li><a className="link-secondary" href="#">Russ Telen</a></li>
