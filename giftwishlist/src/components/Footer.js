1336e52bcec22e0381f172dece51271be090f426
fatmabadri <fbadri@my.bcit.ca>
2020-12-17 10:22:08 -0800

    Capitalized the titles of the Footer section

--- giftwishlist/src/components/Footer.js
+++ giftwishlist/src/components/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends Component {
      <footer className="pt-4 my-md-5 pt-md-5">
        <div className="row">
           <div className="col-6 col-md">
-                <h6>WishBucket</h6>
+                <h6>WISHBUCKET</h6>
               <ul class="list-unstyled text-small">
                 <li><a className="link-secondary" href="#">Our Story</a></li>
                 <li><a className="link-secondary" href="#">Our People</a></li>
@@ -17,7 +17,7 @@ export default class Footer extends Component {
           </div> 
 
           <div className="col-6 col-md">
-              <h6>Features</h6>
+              <h6>FEATURES</h6>
               <ul class="list-unstyled text-small">
                 <li><a className="link-secondary" href="#">Cool stuff</a></li>
                 <li><a className="link-secondary" href="#">Share With Anyone</a></li>
@@ -27,7 +27,7 @@ export default class Footer extends Component {
           </div> 
 
           <div className="col-6 col-md">
-              <h6>Resources</h6>
+              <h6>RESOURCES</h6>
               <ul className="list-unstyled text-small">
                 <li><a className="link-secondary" href="#">iPhone WishList IOS  App</a></li>
                 <li><a className="link-secondary" href="#">Android WishList App</a></li>
@@ -37,7 +37,7 @@ export default class Footer extends Component {
           </div>
 
           <div className="col-6 col-md">
-              <h6>Solutions</h6>
+              <h6>SOLUTIONS</h6>
               <ul className="list-unstyled text-small">
               <li><a className="link-secondary" href="#">Getting Stared</a></li>
                 <li><a className="link-secondary" href="#">Unique Gift Ideas</a></li>
@@ -49,15 +49,14 @@ export default class Footer extends Component {
        </div>
        <hr/>
        <div className="col-12 col-md">
-                <NavLink to="/">
-                <img src={"https://vectr.com/vleddepruna/aVozy6qf2.svg?width=224&height=236&select=aVozy6qf2page0"}
-                  width="30em"
-                />
-                <h6>WishBucket</h6>
-              </NavLink>
-              {/* <h5>Company</h5> */}
-              <small className="d-block mb-3 text-muted">&copy; 2015-2020</small>
-            </div>
+          <NavLink to="/">
+          <img src={"https://vectr.com/vleddepruna/aVozy6qf2.svg?width=224&height=236&select=aVozy6qf2page0"}
+            width="30em"
+          />
+          <h6>WishBucket</h6>
+        </NavLink>
+        <small className="d-block mb-3 text-muted">&copy; 2015-2020</small>
+      </div>
      </footer>
    );
   }
